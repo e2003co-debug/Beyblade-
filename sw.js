478b8cd185aa83cf427f247d5a5ead493c22510d
e2003co-debug <e2003co@gmail.com>
2025-09-26 10:53:52 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = "beyblade-pwa-v1";
+const ASSETS = ["./","./index.html","./manifest.webmanifest"];
+self.addEventListener("install", e=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener("activate", e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=> k===CACHE_NAME? null : caches.delete(k)))));
+});
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=> r || fetch(e.request).then(n=>{
+      return caches.open(CACHE_NAME).then(c=>{ c.put(e.request, n.clone()); return n; });
+    }).catch(()=> caches.match("./index.html")))
+  );
+});
